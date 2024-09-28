2adea8d8e382bb8aa0c4f53be5256d8d3e121591
Markeu11 <marklesther1130@gmail.com>
2024-09-28 11:27:05 +0800

    message

---
+++ src/InputComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'; 
+class InputComponent extends Component { 
+constructor(props) { 
+super(props); 
+this.state = { 
+inputValue: '', 
+}; 
+} 
+handleInputChange = (event) => { 
+this.setState({ 
+inputValue: event.target.value, 
+}); 
+}; 
+render() { 
+return (
+<div> 
+<h2>String Value</h2> 
+<input 
+type="text" 
+placeholder="Enter something..." 
+value={this.state.inputValue} 
+onChange={this.handleInputChange} 
+/> 
+<p>You typed: {this.state.inputValue}</p> 
+</div> 
+); 
+} 
+} 
+export default InputComponent; 
